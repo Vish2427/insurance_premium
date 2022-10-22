1fd9a338877053e5f9f2427b298e0031ee018ea8
Vishal <ranbirrathore150@gmail.com>
2022-10-22 19:32:50 +0530

    'normal'

--- premium/Constant/__init__.py
+++ premium/Constant/__init__.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 
 ROOT_DIR = os.getcwd()
-CONFIG_DIR = "Config"
+CONFIG_DIR = "config"
 CONFIG_FILE_NAME = "config.yaml"
 CONFIG_FILE_PATH = os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
 
