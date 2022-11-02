de2c9099aa26787a56d310ae49bda7e7236082ce
Vishal <ranbirrathore150@gmail.com>
2022-11-02 15:04:00 +0530

    "wrkflw"

--- premium/configuration/mongo_db_connection.py
+++ premium/configuration/mongo_db_connection.py
@@ -3,14 +3,14 @@ from premium.Constant.database import DATABASE_NAME
 import certifi
 import os
 ca = certifi.where()
-#MONGODB_URL_KEY = "MONGODB_URL_KEY"
+MONGODB_URL_KEY = "MONGO_DB_URL"
 class MongoDBClient:
     client = None
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
 
             if MongoDBClient.client is None:
-                mongo_db_url = os.getenv("MONGODB_URL_KEY")
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
                 
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
