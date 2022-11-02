71148b74fe54f671b98278135b270b92ab89ea86
Vishal <ranbirrathore150@gmail.com>
2022-11-02 09:38:19 +0530

    " changed congif to Config"

--- premium/Constant/__init__.py
+++ premium/Constant/__init__.py
@@ -6,7 +6,7 @@ def get_current_time_stamp():
     return f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
 
 ROOT_DIR = os.getcwd()
-CONFIG_DIR = "config"
+CONFIG_DIR = "Config"
 CONFIG_FILE_NAME = "config.yaml"
 CONFIG_FILE_PATH = os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
 
